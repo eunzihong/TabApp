b899f019a9f75aee335e7e22619e6b296730462e
EunjiHong <eunjihong.3698@gmail.com>
2020-07-14 14:36:35 +0900

    Load contacts from device & arrange ContactFragment code

--- app/src/main/java/app/bottomtab/ui/contact/ContactFragment.java
+++ app/src/main/java/app/bottomtab/ui/contact/ContactFragment.java
@@ -1,5 +1,6 @@
 package app.bottomtab.ui.contact;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,6 +20,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import app.bottomtab.R;
 
@@ -32,16 +36,48 @@ public class ContactFragment extends Fragment {
         final androidx.recyclerview.widget.RecyclerView ContactView =
                 root.findViewById(R.id.recyclerview_contacts);
 
+        // Permission setting
+        getPermission(Manifest.permission.READ_CONTACTS);
+
+        // RecyclerView setting
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         ContactView.setLayoutManager(linearLayoutManager);
-
         adapter = new RecyclerAdapter();
         ContactView.setAdapter(adapter);
 
+        // Recycler adapter contact data setting
+        setJSONcontacts(adapter, "contacts.json");  // Load contact data from contacts.json
+        setDeviceContacts(adapter);     // Load the user's contact list
+        adapter.notifyDataSetChanged(); // Notify the adapter data modification
+
+        return root;
+    }
+
+    // Permission request via TedPermission
+    public void getPermission(String permissionId){
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(getActivity(), "Permission Denied: "+deniedPermissions.get(0), Toast.LENGTH_SHORT).show();
+            }
+        };
+        new TedPermission(getActivity())
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage("READ CONTACTS permission required")
+                .setPermissions(permissionId)
+                .check();
+    }
+
+    // Load contact data from json database
+    public void setJSONcontacts(RecyclerAdapter adapter, String filename){
         String json = "";
 
-        try {
-            InputStream is = getActivity().getAssets().open("contacts.json");
+        try {   // Open and load json file
+            InputStream is = getActivity().getAssets().open(filename);
             int fileSize = is.available();
 
             byte[] buffer = new byte[fileSize];
@@ -55,7 +91,7 @@ public class ContactFragment extends Fragment {
             ex.printStackTrace();
         }
 
-        try{
+        try{    // json data parsing
             JSONObject jsonObject = new JSONObject(json);
 
             JSONArray contactArray = jsonObject.getJSONArray("Contacts");
@@ -78,10 +114,19 @@ public class ContactFragment extends Fragment {
             e.printStackTrace();
         }
 
-        adapter.notifyDataSetChanged();
-
-        return root;
     }
 
+    // Load the user's contact list
+    public void setDeviceContacts(RecyclerAdapter adapter){
+        ContactUtil contactUtil = new ContactUtil(getActivity());
+
+        ArrayList<Contact> phoneBook;
+        phoneBook = contactUtil.getContactList();
+
+        for(int i=0; i<phoneBook.size(); i++)
+        {
+            adapter.addItem(phoneBook.get(i));
+        }
+    }
 
 }
